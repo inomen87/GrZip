802eff496a86dcf597adfa17ae54692ce6573cc4
inomen87 <inomen87@gmail.com>
2023-04-01 07:39:42 +0200

    Test

--- main.py
+++ main.py
@@ -12,11 +12,11 @@ for num in range(1, len(inputstring)):
 
 
 outputstring += "(" + str(counter) + "," + inputstring[-1] + ")"
-pinta = (len(outputstring) / len(inputstring)  * 100)
+compress_percent = (len(outputstring) / len(inputstring) * 100)
 
-if (100 - pinta) > 0:
+if (100 - compress_percent) > 0:
 	print(outputstring)
-	print("Compression successful. Space saved: " + str(round(100 - pinta)) + "%")
+	print("Compression successful. Space saved: " + str(round(100 - compress_percent)) + "%")
 else:
 	print("Compression unsuccessful. Input too short.")
 
