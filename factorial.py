17f8624b3fe9d7df3ae35841003c4cfda4e9eb3e
inomen87 <inomen87@gmail.com>
2023-04-06 17:40:25 +0200

    Test

--- factorial.py
+++ factorial.py
@@ -1,14 +1,3 @@
-
-input_num = int(input("Type the number: ")) # TODO: Please move this to main clause. Anything that is only executed
-# when
-# the file is called via python factorial.py has to go into the __main__ part.
-
-counter_1 = 2
-counter_2 = 0
-counter_3 = 1
-control_num = input_num - 2
-
-
 def factorial(n):
     # Global variables are to be avoided whenever possible. Here it is possible to avoid globals.
     # Read https://docs.python.org/3.8/reference/simple_stmts.html#the-global-statement for the global statement.
@@ -26,10 +15,16 @@ def factorial(n):
         factorial(n) # TODO: You are not handling the return of this recursive call !
 
     if counter_3 == 1:
-        print(n) # Print inside a function is bad. the print should go in the __main__ section
+
         counter_3 += 1
+        return n
     # TODO: The function does not return the value of factorial(n) A function that does not return anything is bad
     #  code style in general
 
 if __name__ == '__main__':
+    input_num = int(input("Type the number: "))  #  Please move this to main clause. Anything that is only executed when the file is called via python factorial.py has to go into the __main__ part.
+    control_num = input_num - 2
+    counter_1 = 2
+    counter_2 = 0
+    counter_3 = 1
     factorial(input_num)
