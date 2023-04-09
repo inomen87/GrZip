6c0ceb2e5db999872a78c30e24068fe1f99b976e
inomen87 <inomen87@gmail.com>
2023-04-09 22:01:03 +0200

    Test

--- GrZip/decompression 2.py
+++ GrZip/decompression 2.py
@@ -1,24 +1,27 @@
 def decompress(inputstring):
+    """ Function decompresses an input string which is a compressed alphanumeric string. Input string comes
+    in the following format e.g.: "(4),(X),(5),(c)". In this example 4 and 5 are compression values and X and c are
+    compressed alphanumeric characters. ( and ) are just delimiters. The function then returns a decompressed
+    string in this format: "XXXXccccc". """
     # In first_list we split the input string at commas
     first_list = inputstring.split(",")
     second_list = []
-
+    # We use replace() method to get rid of parenthesis in our compressed string and save result in a list
     for index in first_list:
         second_list.append(index.replace("(", "").replace(")", ""))
 
-    final_list = second_list
-    print(final_list)
+    print(second_list)
 
-    for num in range(len(final_list)):
+    for num in range(len(second_list)):
         # Now thanks to modulo we know that every number that indicates compression level is going to be even
-        if final_list.index(final_list[num]) % 2 == 0:
-            num_char = int(final_list[num]) - 1    # We take that compression level number and turn it into integer
+        if second_list.index(second_list[num]) % 2 == 0:
+            num_char = int(second_list[num]) - 1    # We take that compression level number and turn it into integer
             # We insert alphanumeric characters instead of numbers in the right position
-            final_list.insert(num, final_list[num + 1] * num_char)
-            final_list.pop(num + 1)
+            second_list.insert(num, second_list[num + 1] * num_char)
+            second_list.pop(num + 1)
 
-    final_list = "".join(final_list)  # Finally we will join a list into a string again and print it out
-    return final_list
+    second_list = "".join(second_list)  # Finally we will join a list into a string again and print it out
+    return second_list
 
 print(decompress("(58,X),(666,c),(455,b),(58,B),(666,R),(455,5)"))
 
