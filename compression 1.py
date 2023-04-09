8b21f44236f34ad0732000113ca38fd046bcab31
inomen87 <inomen87@gmail.com>
2023-04-09 21:36:53 +0200

    Test

--- compression 1.py
+++ compression 1.py
@@ -2,30 +2,31 @@ def compress(inputstring, delimiter):
     """ Function compresses an alphanumeric input string(e.g. "zzzzaaaa") and returns
      a string in this format: "{4,z},{4,a}".
      "{" and "}" are delimiters and can be changed in the function arguments.  """
-    if len(delimiter) == 2 and len(inputstring) > 0:
-        outputstring = ""
-        counter = 1
+    if len(delimiter) != 2 or len(inputstring) < 1:
+        return None
 
-        for num in range(1, len(inputstring)):
-            if inputstring[num] == inputstring[num - 1]:
-                counter += 1
-            else:
-                outputstring += delimiter[0] + str(counter) + "," + inputstring[num - 1] + delimiter[1] + ","
-                counter = 1
+    outputstring = ""
+    counter = 1
 
-        outputstring += delimiter[0] + str(counter) + "," + inputstring[-1] + delimiter[1]
-        compress_percent = (len(outputstring) / len(inputstring) * 100)
-
-        if (100 - compress_percent) > 0:
-            return outputstring, "Compression successful. Space saved: " + str(round(100 - compress_percent)) + "%"
+    for num in range(1, len(inputstring)):
+        if inputstring[num] == inputstring[num - 1]:
+            counter += 1
         else:
-            return outputstring, "Compression unsuccessful. Input too short."
+            outputstring += delimiter[0] + str(counter) + "," + inputstring[num - 1] + delimiter[1] + ","
+            counter = 1
 
+    outputstring += delimiter[0] + str(counter) + "," + inputstring[-1] + delimiter[1]
+    compress_percent = (len(outputstring) / len(inputstring) * 100)
+
+    if (100 - compress_percent) > 0:
+        return outputstring, "Compression successful. Space saved: " + str(round(100 - compress_percent)) + "%"
     else:
-        print("Please provide input.")
-        raise ValueError
+        return outputstring, "Compression unsuccessful. Input too short."
 
 
-print(compress("z", delimiter=["{","}"]))
+
+
+
+print(compress("zzzzaaaa", delimiter=["{","}"]))
 
 
--- recursion.py
+++ recursion.py
@@ -5,7 +5,12 @@ WHAT ABOUT
 #
 # LIBRARY CODE (functions, classes, constants,...)
 #
+def recurse(x):
+    if x >= 1:
+        print(x)
+        recurse(x-1)
+
 
 if __name__ == "__main__":
   # FILE CODE / TESTING / DEVELOPMENT
-  pass
+  recurse(10)
