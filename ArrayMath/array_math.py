31185bef395f02b383b21f3219001cf1cbb86319
inomen87 <inomen87@gmail.com>
2023-04-11 00:50:22 +0200

    Added function protection :)

--- ArrayMath/array_math.py
+++ ArrayMath/array_math.py
@@ -1,16 +1,22 @@
 def even_odd(numlist, oddoreven):
-	oddlist = []
-	evenlist = []
-	if oddoreven == "odd":
-		for element in numlist:
-			if element % 2 != 0:
-				oddlist.append(element)
-		return oddlist
-	elif oddoreven == "even":
-		for element in numlist:
-			if element % 2 == 0:
-				evenlist.append(element)
-		return evenlist
+	if isinstance(numlist, list):
+		if isinstance(oddoreven, str):
+			oddlist = []
+			evenlist = []
+			if oddoreven == "odd":
+				for element in numlist:
+					if element % 2 != 0:
+						oddlist.append(element)
+				return oddlist
+			elif oddoreven == "even":
+				for element in numlist:
+					if element % 2 == 0:
+						evenlist.append(element)
+				return evenlist
+		else:
+			return None
+	else:
+		return None
 
 print(even_odd([6 ,7 ,3 ,4 ,5 ,6 ,7 ,8], "odd"))
 
@@ -18,11 +24,14 @@ print(even_odd([6 ,7 ,3 ,4 ,5 ,6 ,7 ,8], "odd"))
 
 
 
-def addition(num_list):
-    totalsum = 0
-    for num in range(len(num_list)):
-        totalsum += num_list[num]
-    return totalsum
+def addition(numlist):
+	if isinstance(numlist, list):
+		totalsum = 0
+		for num in range(len(numlist)):
+			totalsum += numlist[num]
+		return totalsum
+	else:
+		return None
 
 print(addition([6,7,3,4,5,6,7,8]))
 
@@ -31,11 +40,14 @@ print(addition([6,7,3,4,5,6,7,8]))
 
 
 
-def difference(num_list):
-	totalsum = 0
-	for element in num_list:
-		totalsum += element
-	return totalsum
+def difference(numlist):
+	if isinstance(numlist, list):
+		totalsum = 0
+		for element in numlist:
+			totalsum += element
+		return totalsum
+	else:
+		return None
 
 print(difference([6,7,3,4,5,6,7,8]))
 
@@ -45,14 +57,16 @@ print(difference([6,7,3,4,5,6,7,8]))
 
 
 def multiplication(numlist):
-
-	counter = len(numlist)
-	result = 1
-	while counter >= 1:
-		result = result * numlist[0]
-		numlist.pop(0)
-		counter -= 1
-	return result
+	if isinstance(numlist, list):
+		counter = len(numlist)
+		result = 1
+		while counter >= 1:
+			result = result * numlist[0]
+			numlist.pop(0)
+			counter -= 1
+		return result
+	else:
+		return None
 
 print(multiplication([1,2,3,4,5,6,7,8,9]))
 
