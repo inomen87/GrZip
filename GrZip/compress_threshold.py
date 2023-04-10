4449905ed9308c37c204da6b257ab88f7a484c66
Philipp Rollinger <philipp.rollinger@gmail.com>
2023-04-10 16:56:34 +0200

    compress/decompress 2nd specification

--- GrZip/compress_threshold.py
+++ GrZip/compress_threshold.py
@@ -12,13 +12,82 @@ ZaaabbbbbXccccccdefY => Zaaa(5,b)X(6c)defY
 def compress(sequence, threshold=5, delimiter=["(",")"]):
     """ Take a sequence as a string and returns the compressed sequence as a string.
     """
-    return "Zaaa(5,b)X(6c)defY"
+    # VARIABLES
+    compressed_sequence = ""
+    buffer = ""
+
+    # TODO: Parameter Protection (s,t,d)
+
+    def buffer_compress(last=False):
+        # Helper: compress the buffer
+        if len(buffer) >= threshold:
+            # compressed copy return
+            if last:
+                return f"{delimiter[0]}{len(buffer)},{buffer[0]}{delimiter[1]}"
+            else:
+                return f"{delimiter[0]}{len(buffer) - 1},{buffer[0]}{delimiter[1]}"
+        else:
+            # uncompressed copy return
+            if last:
+                return buffer
+            else:
+                return buffer[0:-1]
+
+    # ENGINE
+    for c in sequence:
+        buffer += c
+        if len(buffer) <= 1:
+            continue
+        # Buffer has a character change
+        if buffer[-2] != buffer[-1]:
+            compressed_sequence += buffer_compress()
+            buffer = buffer[-1]
+    # Append remaining buffer
+    compressed_sequence += buffer_compress(last=True)
+
+    # RETURN the expanded sequence
+    return compressed_sequence
 
 def decompress(sequence, delimiter=["(",")"]):
     """ Take a compressed sequence as a string and returns the expanded sequence as a string.
     """
-    return "ZaaabbbbbXccccccdefY"
+    expanded_sequence = ""
+    buffer = ""
+
+    def buffer_decompress():
+        n = ""
+        for b in buffer:
+            if b.isdigit():
+                n += b
+            if b == ',':
+                break
+        return int(n) * buffer[-2]
+
+    # TODO: Parameter Protection (s,d)
+
+    # ENGINE
+    for c in sequence:
+        if buffer == "" and c != delimiter[0]:
+            expanded_sequence += c
+        else:
+            # Buffer Operation
+            buffer += c
+            if buffer[0] == delimiter[0] and buffer[-1] == delimiter[1]:
+                # Buffer write
+                n = ""
+                for b in buffer:
+                    if b.isdigit():
+                        n += b
+                    if b == ',':
+                        break
+                # Write n times the character in the buffer
+                expanded_sequence += buffer_decompress()
+                buffer = ""
+
+    # RETURN
+    return expanded_sequence
 
 if __name__ == "__main__":
-    assert compress("ZaaabbbbbXccccccdefY", 5) == "Zaaa(5,b)X(6c)defY"
-    assert decompress("Zaaa(5,b)X(6c)defY") == "ZaaabbbbbXccccccdefY"
+    #assert compress("aaaaaaaaaa", 5) == "(10,a)"
+    assert compress("ZaaabbbbbXccccccdefY", 5) == "Zaaa(5,b)X(6,c)defY"
+    #assert decompress("Zaaa(5,b)X(6,c)defY") == "ZaaabbbbbXccccccdefY"
