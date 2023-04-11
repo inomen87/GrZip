1a87a45694524b2fc829c1f63f0b1094c45ce1d0
inomen87 <inomen87@gmail.com>
2023-04-11 19:08:32 +0200

    Added function protection

--- ArrayMath/array_math.py
+++ ArrayMath/array_math.py
@@ -1,38 +1,41 @@
 def even_odd(numlist, oddoreven):
-	if isinstance(numlist, list):
-		if isinstance(oddoreven, str):
-			oddlist = []
-			evenlist = []
-			if oddoreven == "odd":
-				for element in numlist:
-					if element % 2 != 0:
-						oddlist.append(element)
-				return oddlist
-			elif oddoreven == "even":
-				for element in numlist:
-					if element % 2 == 0:
-						evenlist.append(element)
-				return evenlist
-		else:
-			return None
-	else:
-		return None
+    if isinstance(numlist, list):
+        if isinstance(oddoreven, str):
+            oddlist = []
+            evenlist = []
+            if oddoreven == "odd":
+                for element in numlist:
+                    if isinstance(element, int):
+                        if element % 2 != 0:
+                            oddlist.append(element)
+                return oddlist
+            elif oddoreven == "even":
+                for element in numlist:
+                    if isinstance(element, int):
+                        if element % 2 == 0:
+                            evenlist.append(element)
+                return evenlist
+        else:
+            return None
+    else:
+        return None
 
-print(even_odd([6 ,7 ,3 ,4 ,5 ,6 ,7 ,8], "odd"))
+print(even_odd([6 ,"o" ,7 ,8], "even"))
 
 
 
 
 def addition(numlist):
-	if isinstance(numlist, list):
-		totalsum = 0
-		for num in range(len(numlist)):
-			totalsum += numlist[num]
-		return totalsum
-	else:
-		return None
+    if isinstance(numlist, list):
+        totalsum = 0
+        for num in range(len(numlist)):
+            if isinstance(numlist[num], int):
+                totalsum += numlist[num]
+        return totalsum
+    else:
+        return None
 
-print(addition([6,7,3,4,5,6,7,8]))
+print(addition([6,"i",8]))
 
 
 
@@ -40,15 +43,16 @@ print(addition([6,7,3,4,5,6,7,8]))
 
 
 def difference(numlist):
-	if isinstance(numlist, list):
-		totalsum = 0
-		for element in numlist:
-			totalsum += element
-		return totalsum
-	else:
-		return None
+    if isinstance(numlist, list):
+        totalsum = 0
+        for element in numlist:
+            if isinstance(element, int):
+                totalsum += element
+        return totalsum
+    else:
+        return None
 
-print(difference([6,7,3,4,5,6,7,8]))
+print(difference([6,"7,3,4,5,6,7",8]))
 
 
 
@@ -56,18 +60,19 @@ print(difference([6,7,3,4,5,6,7,8]))
 
 
 def multiplication(numlist):
-	if isinstance(numlist, list):
-		counter = len(numlist)
-		result = 1
-		while counter >= 1:
-			result = result * numlist[0]
-			numlist.pop(0)
-			counter -= 1
-		return result
-	else:
-		return None
+    if isinstance(numlist, list):
+        counter = len(numlist)
+        result = 1
+        while counter >= 1:
+            if isinstance(numlist[0], int):
+                result = result * numlist[0]
+            numlist.pop(0)
+            counter -= 1
+        return result
+    else:
+        return None
 
-print(multiplication([1,2,3,4,5,6,7,8,9]))
+print(multiplication([2,"2,3,4,5,6,7,8",9]))
 
 
 
@@ -75,6 +80,6 @@ print(multiplication([1,2,3,4,5,6,7,8,9]))
 
 
 def division(numlist):
-	pass
+    pass
 
 print(division([1,2,3,4,5,6,7,8,9]))
