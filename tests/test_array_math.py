c3fc18cbad08f41b5205c5db1e362d13be48ac18
inomen87 <inomen87@gmail.com>
2023-04-13 12:16:38 +0200

    Added test file for Array Math

---
+++ tests/test_array_math.py
@@ -0,0 +1,51 @@
+
+
+
+from ..ArrayMath.array_math import even_odd, addition, multiplication, difference
+
+
+cases_evenodd = (
+    (([6, "o", 7, 8], "even"), [6, 8]),
+    (([6, 1, 3, 4, 8], "odd"), [1, 3]),
+    (([6, 2, 5, 7 ,8], "even"), [6, 2, 8]),
+)
+
+cases_addition = (
+    ([6,"i",8], 14),
+    ([6,1,2,8], 17),
+    ([6,-2,3,8], 15),
+)
+
+cases_multiplication = (
+    ([2,"2,3,4,5,6,7,8",9], 18),
+    ([2,3,4], 24),
+    ([2,-10,3], -60),
+)
+
+cases_difference = (
+    ([2,"2,3,4,5,6,7,8",9], 11),
+    ([2,1,3,9], 15),
+    ([2,5,2,1,9], 19),
+)
+
+
+
+def test_even_odd():
+    for case in cases_evenodd:
+        assert even_odd(case[0]) == case[1]
+
+
+
+def test_addition():
+    for case in cases_addition:
+        assert addition(case[0]) == case[1]
+
+
+def test_multiplication():
+    for case in cases_multiplication:
+        assert multiplication(case[0]) == case[1]
+
+
+def test_difference():
+    for case in cases_difference:
+        assert difference(case[0]) == case[1]
