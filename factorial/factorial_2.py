19b4a1f5e75ee45ae1325c83364869b9445c594e
Philipp Rollinger <philipp.rollinger@gmail.com>
2023-04-27 08:51:35 +0200

    add factorial functions

--- factorial/factorial_2.py
+++ factorial/factorial_2.py
@@ -1,10 +1,15 @@
-def factorial(n):
+def factorial_recursive(n):
+    """DOC STRING"""
+    if n <= 1:
+        return 1
+    else:
+        return (n * (factorial_recursive(n - 1)))
+
+def factorial_loop(n):
+    """DOC STRING"""
     if n <= 1:
         return 1
     for num in range(1, n):
         b = n * num
         n = b
     return b
-
-if __name__ == "__main__":
-    print(factorial(11))
